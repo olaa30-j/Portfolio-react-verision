785f8e7484142170b7c2489a409a26408df0aa77
olaadel <olaadel.967@gmail.com>
2024-09-08 01:49:31 +0300

    update responsive

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -4,14 +4,14 @@ import { useRef, useState } from 'react';
 
 const Aside = () => {
     const asideShow = useRef(null);
-    const [asideToggle , setAsideToggle ] = useState(false)
+    const [asideToggle, setAsideToggle] = useState(false)
 
 
-    const handleAsideTogle = () =>{
-        setAsideToggle( prevValue =>{
-            if(prevValue){
+    const handleAsideTogle = () => {
+        setAsideToggle(prevValue => {
+            if (prevValue) {
                 asideShow.current.style.display = 'none';
-            }else{
+            } else {
                 asideShow.current.style.display = 'block';
             }
             return !prevValue
@@ -20,50 +20,60 @@ const Aside = () => {
 
     return (
         <>
-        <div className= "toggle" onClick={()=> handleAsideTogle()}><i className={` ${asideToggle? '' : 'fa fa-bars' }`}></i></div>
-        <aside ref={asideShow} className='col-lg-3 col-sm-6'>
-        <div className= "toggle" onClick={()=> handleAsideTogle()}><i className={` ${asideToggle? 'fa fa-times' : '' }`}></i></div>
-            <div className={styles.logo}>
-                <NavLink to="" className={styles.logo_link} > <span>Ola.</span>dev</NavLink >
-            </div>
+            {
+                !asideToggle ? (
+                    <div className="toggle" onClick={() => handleAsideTogle()}><i className='fa fa-bars'></i></div>
+                ) :
+                    null
+            }
+            <aside ref={asideShow} className='col-lg-3 col-sm-6'>
+                {
+                    asideToggle ? (
+                        <div className="toggle" onClick={() => handleAsideTogle()}><i className='fa fa-times'></i></div>
 
-            <ul className={styles.nav_list}>
-                <li className={styles.nav_item}>
-                    <NavLink to="" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
-                        <i className="fa fa-home"></i>
-                        Home
-                    </NavLink >
-                </li>
+                    ) : null
+                }
+                <div className={styles.logo}>
+                    <NavLink to="" className={styles.logo_link} > <span>Ola.</span>dev</NavLink >
+                </div>
 
-                <li className={styles.nav_item}>
-                    <NavLink to="about" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
-                        <i className="fa fa-user"></i>
-                        About Me
-                    </NavLink >
-                </li>
+                <ul className={styles.nav_list}>
+                    <li className={styles.nav_item}>
+                        <NavLink to="" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
+                            <i className="fa fa-home"></i>
+                            Home
+                        </NavLink >
+                    </li>
 
-                <li className={styles.nav_item}>
-                    <NavLink to="service" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
-                        <i className="fa fa-list"></i>
-                        Services
-                    </NavLink >
-                </li>
+                    <li className={styles.nav_item}>
+                        <NavLink to="about" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
+                            <i className="fa fa-user"></i>
+                            About Me
+                        </NavLink >
+                    </li>
 
-                <li className={styles.nav_item}>
-                    <NavLink to="portfolio" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
-                        <i className="fa fa-briefcase"></i>
-                        Portfolio
-                    </NavLink >
-                </li>
+                    <li className={styles.nav_item}>
+                        <NavLink to="service" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
+                            <i className="fa fa-list"></i>
+                            Services
+                        </NavLink >
+                    </li>
 
-                <li className={styles.nav_item}>
-                    <NavLink to="contact" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
-                        <i className="fa fa-comments"></i>
-                        Contact Us
-                    </NavLink >
-                </li>
-            </ul>
-        </aside>
+                    <li className={styles.nav_item}>
+                        <NavLink to="portfolio" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
+                            <i className="fa fa-briefcase"></i>
+                            Portfolio
+                        </NavLink >
+                    </li>
+
+                    <li className={styles.nav_item}>
+                        <NavLink to="contact" className={({ isActive }) => isActive ? `${styles.nav_link} ${styles.active}` : `${styles.nav_link}`}>
+                            <i className="fa fa-comments"></i>
+                            Contact Us
+                        </NavLink >
+                    </li>
+                </ul>
+            </aside>
         </>
     )
 }
